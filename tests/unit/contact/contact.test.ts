0f8bc16a7df1302de1255ae239a8dc1620b63c2d
Younes Lahouiti <loller763@gmail.com>
2025-02-02 16:34:11 +0100

    Included the happy path in the test + managed to mock Bun fetch() request

--- tests/unit/contact/contact.test.ts
+++ tests/unit/contact/contact.test.ts
@@ -2,10 +2,33 @@ import { expect, it, beforeAll, afterAll, describe } from "bun:test";
 import { Hono } from "hono";
 import contactRouter from "@api/contact/index";
 import * as HttpStatusCodes from "@utils/http/http-status-codes.utils";
+import env from "@/src/env";
 
 // Create a test app and mount the router
 const app = new Hono();
 app.route("/", contactRouter);
+
+// ? Stubbing Bun fetch() function to avoid unnecessary calls
+globalThis.fetch = async (input: URL | RequestInfo): Promise<Response> => {
+  const url = input instanceof URL ? input : new URL(input as string);
+
+  switch (url.href) {
+    case env.DISCORD_WEBHOOK_URL: {
+      const mockResponse = new Response(
+        JSON.stringify({
+          success: true,
+          message: "Mocked response from external service",
+        }),
+        { status: 200 }
+      );
+
+      return mockResponse;
+    }
+
+    default:
+      return new Response(null, { status: 404 });
+  }
+};
 
 beforeAll(() => {
   console.log("Starting Contact API tests...");
@@ -124,26 +147,26 @@ describe("Contact endpoint test", () => {
       );
     });
 
-    // it("(HAPPY PATH) should process a valid contact form and return a success response", async () => {
-    //   const body = {
-    //     firstName: "John",
-    //     lastName: "Doe",
-    //     email: "john.doe@example.com",
-    //     projectIdea:
-    //       "A new innovative project idea that is long enough to meet validation requirements.",
-    //   };
+    it("(HAPPY PATH) should process a valid contact form and return a success response", async () => {
+      const body = {
+        firstName: "John",
+        lastName: "Doe",
+        email: "john.doe@example.com",
+        projectIdea:
+          "A new innovative project idea that is long enough to meet validation requirements, Bun + Hono happy path test.",
+      };
 
-    //   const res = await app.request("/contact", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify(body),
-    //   });
+      const res = await app.request("/contact", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
 
-    //   expect(res.status).toBe(HttpStatusCodes.OK);
-    //   const json = await res.json();
+      expect(res.status).toBe(HttpStatusCodes.OK);
+      const json = await res.json();
 
-    //   expect(json.success).toBe(true);
-    //   expect(json.message).toBe("Message sent successfully");
-    // });
+      expect(json.success).toBe(true);
+      expect(json.message).toBe("Message sent successfully");
+    });
   });
 });
