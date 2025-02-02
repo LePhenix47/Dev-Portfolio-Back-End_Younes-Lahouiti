ff5eaca66831f5bb831b2df95bc3cb088c18a2d3
Younes Lahouiti <loller763@gmail.com>
2025-02-02 11:32:14 +0100

    Successfully extracted the controller for the route handler to a different file and made it type safe

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,15 +1,13 @@
-import env from "@src/env";
-
 import { createRouter } from "@src/utils/hono-handlers/router.utils";
 
-import { createMiddleware } from "hono/factory";
+// import { createMiddleware } from "hono/factory";
 
 import { z, createRoute } from "@hono/zod-openapi";
 import { zValidator } from "@hono/zod-validator";
 
-import type { ContentfulStatusCode } from "hono/utils/http-status";
-
 import * as HttpStatusCodes from "@utils/http/http-status-codes.utils";
+
+import * as contactControllers from "@controllers/contact.controllers";
 
 const contactFormSchema = z
   .object({
@@ -43,7 +41,7 @@ const contactFormSchema = z
   })
   .openapi("ContactForm");
 
-type ContactForm = z.infer<typeof contactFormSchema>;
+export type ContactForm = z.infer<typeof contactFormSchema>;
 
 const contactRoute = createRoute({
   path: "/contact",
@@ -99,62 +97,10 @@ const contactRoute = createRoute({
   },
 });
 
-type ContactRouteType = typeof contactRoute;
+export type ContactRouteType = typeof contactRoute;
 
-const contactRouter = createRouter().openapi(contactRoute, async (c) => {
-  try {
-    const payLoad: ContactForm = c.req.valid("json");
-
-    const { firstName, lastName, email, projectIdea } = payLoad;
-
-    const discordPostRequestPayload = {
-      embeds: [
-        {
-          title: `Project idea from ${firstName} ${lastName}:`,
-          description: projectIdea,
-          color: 6448228,
-          author: { name: `Email: ${email}` },
-        },
-      ],
-    };
-
-    const discordPostOptions: RequestInit = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(discordPostRequestPayload),
-    };
-
-    const webhookUrl = new URL(env?.DISCORD_WEBHOOK_URL!);
-
-    const res = await fetch(webhookUrl, discordPostOptions);
-    if (!res.ok) {
-      console.error(res);
-
-      return c.json(
-        {
-          success: false,
-          error: `Failed to send data to Discord. Status: ${res.status}`,
-        },
-        HttpStatusCodes.BAD_REQUEST
-      );
-    }
-
-    const data: {} = await res.json();
-
-    console.log(data);
-    return c.json(
-      { message: "Message sent successfully", success: true },
-      HttpStatusCodes.OK
-    );
-  } catch (error) {
-    console.error(error);
-    return c.json(
-      { success: false, error: "An error occurred" },
-      HttpStatusCodes.INTERNAL_SERVER_ERROR
-    );
-  }
-});
+const contactRouter = createRouter().openapi(
+  contactRoute,
+  contactControllers.contactController
+);
 export default contactRouter;
